2c365472d6401e5a5c7c01a72bf777551132eb5b
Lucas <Lucas@DESKTOP-U5QBEMH.local>
2024-12-29 15:43:10 -0300

    Criando tela

--- pong/src/pong/Game.java
+++ pong/src/pong/Game.java
@@ -12,13 +12,21 @@ import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
+	public static int WIDTH = 160, HEIGHT = 120, SCALE = 3;
 	
 	public Game() {
-		
+		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 	}
 	
 	public static void main(String[] args) {
 		Game game = new Game();
+		JFrame frame = new JFrame("Pong");
+		frame.add(game);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	@Override
