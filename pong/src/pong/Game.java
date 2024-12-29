7b995c7f061ee4a9233fd5a7b3a4a011aa7a5ddb
Lucas <Lucas@DESKTOP-U5QBEMH.local>
2024-12-29 15:55:07 -0300

    Adicionando game looping e graficos

--- pong/src/pong/Game.java
+++ pong/src/pong/Game.java
@@ -13,6 +13,7 @@ import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 	public static int WIDTH = 160, HEIGHT = 120, SCALE = 3;
+	public BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
@@ -27,10 +28,40 @@ public class Game extends Canvas implements Runnable {
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		new Thread(game).start();;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = image.getGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
 	}
 
 	@Override
 	public void run() {
-		
+		while(true) {
+			tick();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
