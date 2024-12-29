e0c20af3ffd620728419c298d5efda9f7384a5f6
Lucas <Lucas@DESKTOP-U5QBEMH.local>
2024-12-29 15:37:16 -0300

    Primeiro commit

---
+++ pong/src/pong/Game.java
@@ -0,0 +1,28 @@
+package pong;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	
+	public Game() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+	}
+
+	@Override
+	public void run() {
+		
+	}
+}
